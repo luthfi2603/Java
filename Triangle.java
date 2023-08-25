50d8d713eefbd5f7efcb40ddb0cfd1834e2f2964
luthfi2603 <luthfim904@gmail.com>
2023-08-25 20:41:16 +0700

    merubah susunan repositori

---
+++ Triangle.java
@@ -0,0 +1,15 @@
+public class Triangle {
+    public static void main(String[] args) {
+        int n = 10;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = n; j > i; j--){
+                System.out.print("  ");
+            }
+            for(int k = 1; k <= (2 * i - 1); k++){
+                System.out.print("x ");
+            }
+            System.out.println();
+        }
+    }
+}
