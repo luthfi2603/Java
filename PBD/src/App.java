c6bdfd905e536550860ca6ab5695116b55e322e0
luthfi2603 <luthfim904@gmail.com>
2023-11-12 13:12:17 +0700

    memindahkan tampilan dari app ke cview

--- PBD/src/App.java
+++ PBD/src/App.java
@@ -1,51 +1,7 @@
-import java.util.Scanner;
 import com.view.cView;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
-        String pilihan;
-
-        while(true){
-            System.out.print("\n========== MENU ==========\n"
-            + "1. Lihat Semua Data Barang\n"
-            + "2. Detail Data Barang\n"
-            + "3. Cari Data Barang\n"
-            + "4. Tambah Data Barang\n"
-            + "5. Update Data Barang\n"
-            + "0. Exit\n"
-            + "Pilih [1/2/3/4/5/0] : ");
-            pilihan = input.nextLine();
-
-            switch(pilihan){
-                case "1":
-                    cView.getAll();
-                    break;
-                case "2":
-                    cView.detailBarang();
-                    break;
-                case "3":
-                    cView.cariData();
-                    break;
-                case "4":
-                    cView.tambahData();
-                    break;
-                case "5":
-                    cView.updateData();
-                    break;
-                case "0":
-                    System.out.println("\nExit");
-                    break;
-                default:
-                    System.out.println("Pilihan anda salah!");
-                    break;
-            }
-
-            if(pilihan.equalsIgnoreCase("0")){
-                break;
-            }
-        }
-
-        input.close();
+        cView.index();
     }
 }
--- PBD/src/com/view/cView.java
+++ PBD/src/com/view/cView.java
@@ -5,7 +5,49 @@ import java.util.Scanner;
 public class cView {
     private static Scanner input = new Scanner(System.in);
     private static int id, stokBarang, hargaBarang;
-    private static String keyword, namaBarang, stokBarangS, hargaBarangS;
+    private static String keyword, namaBarang, stokBarangS, hargaBarangS, pilihan;
+
+    public static void index(){
+        while(true){
+            System.out.print("\n========== MENU ==========\n"
+            + "1. Lihat Semua Data Barang\n"
+            + "2. Detail Data Barang\n"
+            + "3. Cari Data Barang\n"
+            + "4. Tambah Data Barang\n"
+            + "5. Update Data Barang\n"
+            + "0. Exit\n"
+            + "Pilih [1/2/3/4/5/0] : ");
+            pilihan = input.nextLine();
+
+            switch(pilihan){
+                case "1":
+                    cView.getAll();
+                    break;
+                case "2":
+                    cView.detailBarang();
+                    break;
+                case "3":
+                    cView.cariData();
+                    break;
+                case "4":
+                    cView.tambahData();
+                    break;
+                case "5":
+                    cView.updateData();
+                    break;
+                case "0":
+                    System.out.println("\nExit");
+                    break;
+                default:
+                    System.out.println("Pilihan anda salah!");
+                    break;
+            }
+
+            if(pilihan.equalsIgnoreCase("0")){
+                break;
+            }
+        }
+    }
 
     public static void getAll(){
         // pesan header
