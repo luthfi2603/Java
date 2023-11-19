a2f1b036b18c2f61a502346a5919e9f009902bde
luthfi2603 <luthfim904@gmail.com>
2023-11-19 13:31:20 +0700

    belajar gui pertama kali

---
+++ JavaGUI/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        MainFrame myFrame = new MainFrame();
+        myFrame.initialize();
+    }
+}
---
+++ Time.java
@@ -0,0 +1,15 @@
+// import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
+// import java.text.simpleDateFormat;
+// import java.util.Locale;
+// import javax.swing.Timer;
+
+public class Time extends javax.swing.JFrame {
+    public Time(){
+        // initComponents();
+    }
+
+    public void tampilJam(){
+
+    }
+}
