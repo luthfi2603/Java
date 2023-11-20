17ad4e7bf1fd3077817c4cd2c8eb1c9e7cf65807
luthfi2603 <luthfim904@gmail.com>
2023-11-20 20:54:00 +0700

    belajar jradio dan jcheckbox

--- JavaGUI/src/App.java
+++ JavaGUI/src/App.java
@@ -5,8 +5,13 @@ import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
+
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Dimension;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -39,8 +44,9 @@ public class App {
         header.setLayout(null);
 
         JPanel content = new JPanel();
-        content.setBounds(200, 70, 938, 570);
         content.setBackground(new Color(236, 244, 214));
+        content.setMaximumSize(new Dimension(938, Integer.MAX_VALUE));
+        content.setPreferredSize(new Dimension(938, 1000));
         content.setLayout(null);
 
         // component
@@ -76,6 +82,46 @@ public class App {
         JScrollPane scrollTxtAlamat = new JScrollPane(txtAlamat);
         scrollTxtAlamat.setBounds(20, (int) alamat.getLocation().getY() + 29, 500, 150);
 
+        JLabel jenisKelamin = new JLabel("Jenis Kelamin");
+        jenisKelamin.setBounds(20, (int) scrollTxtAlamat.getLocation().getY() + 170, 300, 24);
+        jenisKelamin.setFont(new Font("Times New Roman", Font.PLAIN, 24));
+        
+        JRadioButton rdPria = new JRadioButton("Pria");
+        rdPria.setBounds(20, (int) jenisKelamin.getLocation().getY() + 29, 100, 30);
+        rdPria.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        rdPria.setBackground(new Color(236, 244, 214));
+        
+        JRadioButton rdPerempuan = new JRadioButton("Perempuan");
+        rdPerempuan.setBounds(120, (int) jenisKelamin.getLocation().getY() + 29, 130, 30);
+        rdPerempuan.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        rdPerempuan.setBackground(new Color(236, 244, 214));
+
+        ButtonGroup groupJenisKelamin = new ButtonGroup();
+        groupJenisKelamin.add(rdPria);
+        groupJenisKelamin.add(rdPerempuan);
+
+        JLabel hobi = new JLabel("Hobi");
+        hobi.setBounds(20, (int) rdPerempuan.getLocation().getY() + 50, 300, 24);
+        hobi.setFont(new Font("Times New Roman", Font.PLAIN, 24));
+
+        JCheckBox chMancing = new JCheckBox("Mancing");
+        chMancing.setBounds(20, (int) hobi.getLocation().getY() + 29, 130, 30);
+        chMancing.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        chMancing.setBackground(new Color(236, 244, 214));
+        
+        JCheckBox chGame = new JCheckBox("Ngegame");
+        chGame.setBounds(150, (int) hobi.getLocation().getY() + 29, 130, 30);
+        chGame.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        chGame.setBackground(new Color(236, 244, 214));
+        
+        JCheckBox chTouring = new JCheckBox("Touring");
+        chTouring.setBounds(280, (int) hobi.getLocation().getY() + 29, 130, 30);
+        chTouring.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        chTouring.setBackground(new Color(236, 244, 214));
+
+        JScrollPane scrollContent = new JScrollPane(content);
+        scrollContent.setBounds(200, 70, 938, 570);
+
         // finally
         content.add(title);
         content.add(nama);
@@ -84,9 +130,16 @@ public class App {
         content.add(txtPassword);
         content.add(alamat);
         content.add(scrollTxtAlamat);
+        content.add(jenisKelamin);
+        content.add(rdPria);
+        content.add(rdPerempuan);
+        content.add(hobi);
+        content.add(chMancing);
+        content.add(chGame);
+        content.add(chTouring);
         mainWindow.add(sidebar);
         mainWindow.add(header);
-        mainWindow.add(content);
+        mainWindow.add(scrollContent);
         mainWindow.setVisible(true);
     }
 }
