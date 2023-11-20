54072aad9341ece1b34321fc6f860e7f6d6fd9fc
luthfi2603 <luthfim904@gmail.com>
2023-11-20 12:27:57 +0700

    belajar jlabel dan jtextfield

--- JavaGUI/src/App.java
+++ JavaGUI/src/App.java
@@ -1,39 +1,65 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 import java.awt.Color;
+import java.awt.Font;
 
 public class App {
     public static void main(String[] args) throws Exception {
         /* MainFrame myFrame = new MainFrame();
         myFrame.initialize(); */
+        // mainWindow.setSize(853, 480);
+        // card.setSize(500, 600);
+        // card.setLocation(100, 50);
+        // 1280 x 720 aslinya 1280 x 696, 24px untuk title bar
+        // mainWindow.setExtendedState(JFrame.MAXIMIZED_BOTH); untuk langsung fullscreen ketika dijalankan
 
         // frame
         JFrame mainWindow = new JFrame();
-        mainWindow.setSize(650, 650);
-        // mainWindow.setSize(853, 480);
+        mainWindow.setSize(1138, 640);
         mainWindow.setTitle("Frame Utama");
         mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainWindow.setLayout(null);
         mainWindow.setResizable(false);
-        mainWindow.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        mainWindow.setLocationRelativeTo(null);
         
         // panel
-        JPanel bg = new JPanel();
-        bg.setSize(1280, 696);
-        bg.setBackground(Color.BLUE);
-        bg.setLayout(null);
+        JPanel sidebar = new JPanel();
+        sidebar.setSize(200, 616);
+        sidebar.setBackground(new Color(38, 80, 115));
+        sidebar.setLayout(null);
+        
+        JPanel header = new JPanel();
+        header.setBounds(200, 0, 938, 70);
+        header.setBackground(new Color(45, 149, 150));
+        header.setLayout(null);
 
-        JPanel card = new JPanel();
-        // card.setSize(500, 600);
-        // card.setLocation(100, 50);
-        // 1280 x 720 aslinya 1280 x 696, 24px untuk title bar
-        card.setBounds(400, 108, 480, 480);
-        card.setBackground(Color.WHITE);
-        card.setLayout(null);
+        JPanel content = new JPanel();
+        content.setBounds(200, 70, 938, 570);
+        content.setBackground(new Color(236, 244, 214));
+        content.setLayout(null);
+
+        // component
+        JLabel title = new JLabel("Isi Data User");
+        title.setBounds(20, 20, 300, 30);
+        title.setFont(new Font("Times New Roman Bold", Font.PLAIN, 30));
+        
+        JLabel nama = new JLabel("Nama");
+        nama.setBounds(20, 70, 300, 20);
+        nama.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        
+        JTextField txtNama = new JTextField();
+        txtNama.setBounds(20, 95, 300, 30);
+        txtNama.setFont(new Font("Times New Roman", Font.PLAIN, 14));
 
         // finally
-        bg.add(card);
-        mainWindow.add(bg);
+        content.add(title);
+        content.add(nama);
+        content.add(txtNama);
+        mainWindow.add(sidebar);
+        mainWindow.add(header);
+        mainWindow.add(content);
         mainWindow.setVisible(true);
     }
 }
