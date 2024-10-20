417f499b984424d9053d469b67f585a988407e9c
luthfi2603 <luthfim904@gmail.com>
2024-10-20 15:06:41 +0700

    buat template frame login dan registrasi

---
+++ PBD-Project/src/com/partials/cFonts.java
@@ -0,0 +1,7 @@
+package com.partials;
+
+import java.awt.Font;
+
+public class cFonts {
+    public static final Font TITLE_START_FONT = new Font("Poppins Semibold", Font.PLAIN, 26);
+}
--- PBD-Project/src/com/program/App.java
+++ PBD-Project/src/com/program/App.java
@@ -1,20 +1,10 @@
 package com.program;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import com.partials.cColor;
+import com.templates.cStartFrame;
 
 public class App {
     public static void main(String[] args) throws Exception{
-        JFrame myFrame = new JFrame();
-        myFrame.setSize(600, 400);
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JPanel panel1 = new JPanel();
-        panel1.setBounds(0, 0, 600, 400);
-        panel1.setBackground(cColor.GRAY);
-
-        myFrame.add(panel1);
-        myFrame.setVisible(true);
+        cStartFrame startFrame = new cStartFrame("Template Start");
+        startFrame.setVisible(true);
     }
 }
